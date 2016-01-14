025ade83c56ccd2bccfac58373d9e49b2be3793d
Leo <leoshch@gmail.com>
2016-01-15 00:19:20 +0800

    Create create_primarykey_table.sql

---
+++ sqlscripts/create_primarykey_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[info3](
+	[Name] [nvarchar](50) NULL,
+	[CtfId] [nvarchar](50) NULL,
+	[Gender] [nvarchar](50) NULL,
+	[Birthday] [nvarchar](50) NULL,
+	[Address] [nvarchar](100) NULL,
+	[District3] [nvarchar](50) NULL,
+	[District4] [nvarchar](50) NULL,
+	[Mobile] [nvarchar](50) NULL,
+	[Tel] [nvarchar](50) NULL,
+	[EMail] [nvarchar](60) NULL,
+	[Family] [nvarchar](50) NULL,
+	[Version] [nvarchar](50) NULL,
+	[id] [int] primary key NOT NULL
+) ON [PRIMARY]
